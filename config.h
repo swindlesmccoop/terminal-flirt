3c8ca086b708bab3640422ef3eb9ef0a626a3d17
Kusoneko <kusoneko@kusoneko.moe>
2023-05-24 01:34:29 -0400

    Fix define not actually changing terminal colors

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define GREEN "\033[32m"
 #define YELLOW "\033[33m"
 #define BLUE "\033[36m"
-#define NORMAL "[0m"
+#define NORMAL "\033[0m"
 
 //#define STRINGS "tsundere"
 //#define STRINGS "imouto"
