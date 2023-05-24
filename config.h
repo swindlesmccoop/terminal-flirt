57a01a0c463eded55ccd95271fc49d8d1eb50679
Kusoneko <kusoneko@kusoneko.moe>
2023-05-24 01:27:58 -0400

    Define the return to normal color

--- config.h
+++ config.h
@@ -2,9 +2,11 @@
 #define GREEN "\033[32m"
 #define YELLOW "\033[33m"
 #define BLUE "\033[36m"
+#define NORMAL "[0m"
 
 //#define STRINGS "tsundere"
 //#define STRINGS "imouto"
 //#define STRINGS "maid"
 //#define STRINGS "onee"
 //#define STRINGS "kouhai"
+
