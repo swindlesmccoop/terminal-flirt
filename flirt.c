733c099fc3f8469a9e955a80e939f9d4160060c9
swindlesmccoop <swindlesmccoop@gmail.com>
2023-05-30 03:45:38 +0000

    Separate strings

--- flirt.c
+++ flirt.c
@@ -6,11 +6,36 @@
 
 int main() {
 	const char* colors[] = { RED, GREEN, YELLOW, BLUE };
-	const char* tsundere_strings[] = { "It's not like I wanted to do it for you or anything.", "I'm not doing this for you again.", "Ugh, what a bother." };
-	const char* imouto_strings[] = { "I'll take care of it, onii-chan!", "Onii-chan, don't worry about it.", "Leave it to me, onii-chan!" };
-	const char* maid_strings[] = { "As you wish.", "I'll do it for you.", "Yes, master." };
-	const char* onee_strings[] = { "My, my, I'll do it for you.", "I think I can handle it this time.", "I'll make sure this executes properly." };
-	const char* kouhai_strings[] = { "I'll do it, senpai.", "This will be a breeze.", "Senpai, I'll take care of it for you." };
+	
+	const char* tsundere_strings[] = {
+		"It's not like I wanted to do it for you or anything.",
+		"I'm not doing this for you again.",
+		"Ugh, what a bother."
+	};
+	
+	const char* imouto_strings[] = {
+		"I'll take care of it, onii-chan!",
+		"Onii-chan, don't worry about it.",
+		"Leave it to me, onii-chan!"
+	};
+	
+	const char* maid_strings[] = {
+		"As you wish.",
+		"I'll do it for you.",
+		"Yes, master."
+	};
+	
+	const char* onee_strings[] = {
+		"My, my, I'll do it for you.",
+		"I think I can handle it this time.",
+		"I'll make sure this executes properly."
+	};
+	
+	const char* kouhai_strings[] = {
+		"I'll do it, senpai.",
+		"This will be a breeze.",
+		"Senpai, I'll take care of it for you."
+	};
 
 	//select the array of strings based on the STRINGS macro
 	const char** strings;
